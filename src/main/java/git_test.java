987d5119add89e3cfc78406be3d3ba34615d5a07
Wukoooo <1767666580@qq.com>
2022-05-04 21:27:29 +0800

    测试IDEA集成Git,提交

--- src/main/java/git_test.java
+++ src/main/java/git_test.java
@@ -2,5 +2,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git 2");
+        System.out.println("Hello Git 3");
     }
 }
